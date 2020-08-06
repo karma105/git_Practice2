2fa88573231401c3e1312af1a58e6739d6ff90db
karma105 <karim.mammadov@gmail.com>
2020-08-05 21:21:44 -0400

    MissJava.java

--- generateGmail/src/Test/SendGmail.java
+++ generateGmail/src/Test/SendGmail.java
@@ -1,4 +1,37 @@
 package Test;
 
+import Library.Util;
+
 public class SendGmail {
+    /*
+            first Step:
+                go to  :
+                     https://myaccount.google.com/lesssecureapps?utm_source=google-account&utm_medium=web
+                        Make sure that your google account allows third part app
+ */
+    public static void main(String[] args) {
+        // credentials
+        String userName = "";
+        String passWord = "";
+        // email:
+        String[] receivers = {};
+        String subject = "";
+        String text = "";
+        int times = 5;
+        int count  = 1;
+        while(times > 0){ //send the email multiple time
+            for( String each : receivers ){  // for sending emails to multiple people
+                System.out.println("Sending to: "+each  );
+                Util.sendEmails(userName,passWord,each, subject,text);
+            }
+            System.out.println(count++);
+            times--;
+        }
+        System.out.println("Completed");
+    }
 }
+
+
+
+
+
